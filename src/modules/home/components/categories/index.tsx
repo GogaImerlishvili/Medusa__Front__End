5fb2fa791aa4e8dbc9f6d7921d1115ce45aa47ee
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-11-18 13:37:08 +0400

    Refactor and fix css in categories component

---
+++ src/modules/home/components/categories/index.tsx
@@ -0,0 +1,37 @@
+import Image, { StaticImageData } from "next/image"
+import menClothes from "./images/839b3ef63d3f0d537286f4f5bd2da4be.jpg"
+import sales from "./images/5173c434d7b50235b151ac58118b1a09.jpg"
+import onlineExcl from "./images/jhdhgfjgh20220611100951.jpg"
+import fashion from "./images/new-collection-clothes-mall-neatly-folded-high-quality-photo-new-collection-clothes-mall-neatly-folded-206548715.jpg"
+
+interface CategoryProps {
+  title: string
+  imgSrc: StaticImageData
+}
+
+const Category = ({ title, imgSrc }: CategoryProps) => (
+  <li className="mx-auto ">
+    <Image
+      src={imgSrc}
+      className="object-fill "
+      width="400"
+      height="350"
+      alt="men clothes"
+    />
+    <h1 className="text-center relative">{title}</h1>
+  </li>
+)
+
+const Categories = () => {
+  return (
+    <ul className="p-2 flex gap-x-2 flex-col md:flex-row">
+      <Category imgSrc={menClothes} title="ფასდაკლება" />
+      <Category imgSrc={sales} title="საშემოდგომო კოლექცია" />
+      <Category imgSrc={sales} title="ონლაინ ექკლუზივი" />
+      <Category imgSrc={onlineExcl} title="ჩანთები და აქსესუარები" />
+      <Category imgSrc={fashion} title="ახალი კოლექცია" />
+    </ul>
+  )
+}
+
+export default Categories
--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,65 +1,34 @@
 import UnderlineLink from "@modules/common/components/underline-link"
 import Image from "next/image"
-import menClothes from "../hero/images/839b3ef63d3f0d537286f4f5bd2da4be.jpg"
-import newCollection from "../hero/images/images (1).jpeg"
-import sales from "../hero/images/5173c434d7b50235b151ac58118b1a09.jpg"
-import onlineExcl from "../hero/images/jhdhgfjgh20220611100951.jpg"
-import fashion from "../hero/images/new-collection-clothes-mall-neatly-folded-high-quality-photo-new-collection-clothes-mall-neatly-folded-206548715.jpg"
-import styles from "./images.module.css"
+import Categories from "../categories/index"
 
 const Hero = () => {
   return (
     <>
-    <div className="h-[90vh] w-full relative">
-      <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
-        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Summer styles are finally here!! goga && kaxa
-        </h1>
-        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This year, our new summer collection will shelter you from the harsh
-          elements of a world that doesn&apos;t care if you live or die.
-        </p>
-        <UnderlineLink href="/store">Explore products</UnderlineLink>
+      <div className="h-[90vh] w-full relative">
+        <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+          <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
+            Summer styles are finally here!! goga && kaxa
+          </h1>
+          <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
+            This year, our new summer collection will shelter you from the harsh
+            elements of a world that doesn&apos;t care if you live or die.
+          </p>
+          <UnderlineLink href="/store">Explore products</UnderlineLink>
+        </div>
+        <Image
+          src="/hero.jpg"
+          layout="fill"
+          loading="eager"
+          priority={true}
+          quality={90}
+          objectFit="cover"
+          alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
+          className="absolute inset-0"
+          draggable="false"
+        />
       </div>
-      <Image
-        src="/hero.jpg"
-        layout="fill"
-        loading="eager"
-        priority={true}
-        quality={90}
-        objectFit="cover"
-        alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
-        className="absolute inset-0"
-        draggable="false"
-      />
-    </div>
-    <div className="h-[38vh] w-[90%] ml-[5%] relative  mt-4">
-    <ul className="grid grid-cols-5 small:grid-flow-col gap-x-2 ">
-      <li key={"1"}>
-      <Image src={menClothes} alt="men clothes" height={366} />
-      <h1 className="title">ფასდაკლება</h1>
-      </li>
-      <li key={"2"}>
-      <Image src={newCollection} alt="new collection"  height={280} />
-      <h1 className="title">საშემოდგომო კოლექცია</h1>
-      </li>
-      <li key={"2"}>
-      <Image src={sales} alt="new collection"   />
-      <h1 className="title">ონლაინ ექკლუზივი</h1>
-      </li>
-      <li key={"2"}>
-      <Image src={onlineExcl} alt="new collection" height={600}  />
-      <h1 className="title">ჩანთები და აქსესუარები</h1>
-      </li>
-      <li key={"2"}>
-      <Image src={fashion} alt="new collection"  height={800}/>
-      <h1 className={styles.title1}>ახალი კოლექცია</h1>
-      </li>
-     
-
-    </ul>
-        
-    </div>
+      <Categories />
     </>
   )
 }
