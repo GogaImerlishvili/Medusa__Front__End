7b60c0f849e9a590c2e8cfa51d236f1a7c897b68
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-12-03 15:13:03 +0400

    add placeholder links in categories menu

--- src/modules/home/components/categories/index.tsx
+++ src/modules/home/components/categories/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 const Categories = () => {
   return (
-    <div className="h-[85vh] relative w-1/6 bg-gray m-2 flex flex-col justify-items-center text-center">
+    <div className="h-[85vh] relative w-1/6 bg-gray  flex flex-col justify-items-center text-center">
       <Link href="/hats">
         <a className="p-5 hover:text-green-500 ease-in duration-200">Hats</a>
       </Link>
@@ -24,8 +24,27 @@ const Categories = () => {
         More Categories
       </button>
 
-      <div className="hidden peer-hover:flex hover:flex absolute z-20 -right-[82vw] bg-red-500 h-[85vh] w-[82vw] mr-1 ">
-        list of categories
+      <div className="hidden peer-hover:flex hover:flex absolute z-20 -right-[82vw] top-2 bg-red-500 h-[85vh] w-[82vw] flex-col flex-wrap">
+        <Link href="/hats">
+          <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4">
+            Hats
+          </a>
+        </Link>
+        <Link href="/hats">
+          <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4">
+            Hats
+          </a>
+        </Link>
+        <Link href="/hats">
+          <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4">
+            Hats
+          </a>
+        </Link>
+        <Link href="/hats">
+          <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4 mr-auto">
+            Hats
+          </a>
+        </Link>
       </div>
     </div>
   )
