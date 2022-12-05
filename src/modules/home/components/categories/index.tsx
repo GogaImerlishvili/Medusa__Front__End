3f2e7f5b61dc8a6cc7a6444cb79f311bd54d12cc
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-12-05 20:45:20 +0400

    add basic responsiveness to categories

--- src/modules/home/components/categories/index.tsx
+++ src/modules/home/components/categories/index.tsx
@@ -1,52 +1,55 @@
 import Link from "next/link"
 const Categories = () => {
   return (
-    <div className="h-[85vh] relative w-1/6 bg-gray  flex flex-col justify-items-center text-center">
-      <Link href="/hats">
-        <a className="p-5 hover:text-green-500 ease-in duration-200">Hats</a>
-      </Link>
-      <Link href="/hats">
-        <a className="p-5 hover:text-green-500 duration-200 ease-out">Hats</a>
-      </Link>
-      <Link href="/hats">
-        <a className="p-5 hover:text-green-500 ease-in duration-200">Hats</a>
-      </Link>
-      <Link href="/hats">
-        <a className="p-5 hover:text-green-500 duration-200 ease-out">Hats</a>
-      </Link>
-      <Link href="/hats">
-        <a className="p-5 hover:text-green-500 ease-in duration-200">Hats</a>
-      </Link>
-      <Link href="/hats">
-        <a className="p-5 hover:text-green-500 duration-200 ease-out">Hats</a>
-      </Link>
-      <button className="peer absolute w-full bottom-2 text-center">
-        More Categories
-      </button>
+    <>
+      <button className="peer md:hidden">categories hamburger menu</button>
+      <div className="peer-hover:hidden w-full md:h-[85vh] relative md:w-1/6 bg-gray flex flex-col md:justify-items-center text-center">
+        <Link href="/hats">
+          <a className="p-5 hover:text-green-500 ease-in duration-200">Hats</a>
+        </Link>
+        <Link href="/hats">
+          <a className="p-5 hover:text-green-500 duration-200 ease-out">Hats</a>
+        </Link>
+        <Link href="/hats">
+          <a className="p-5 hover:text-green-500 ease-in duration-200">Hats</a>
+        </Link>
+        <Link href="/hats">
+          <a className="p-5 hover:text-green-500 duration-200 ease-out">Hats</a>
+        </Link>
+        <Link href="/hats">
+          <a className="p-5 hover:text-green-500 ease-in duration-200">Hats</a>
+        </Link>
+        <Link href="/hats">
+          <a className="p-5 hover:text-green-500 duration-200 ease-out">Hats</a>
+        </Link>
+        <button className=" peer md:absolute w-full md:bottom-2 text-center">
+          More Categories
+        </button>
 
-      <div className="hidden peer-hover:flex hover:flex absolute z-20 -right-[82vw] top-2 bg-red-500 h-[85vh] w-[82vw] flex-col flex-wrap">
-        <Link href="/hats">
-          <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4">
-            Hats
-          </a>
-        </Link>
-        <Link href="/hats">
-          <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4">
-            Hats
-          </a>
-        </Link>
-        <Link href="/hats">
-          <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4">
-            Hats
-          </a>
-        </Link>
-        <Link href="/hats">
-          <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4 mr-auto">
-            Hats
-          </a>
-        </Link>
+        <div className="hidden md:peer-hover:flex hover:flex absolute z-20 -right-[82vw] top-2 bg-white h-[85vh] w-[82vw] flex-col flex-wrap peer-hover:drop-shadow-lg hover:drop-shadow-lg">
+          <Link href="/hats">
+            <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4">
+              Hats
+            </a>
+          </Link>
+          <Link href="/hats">
+            <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4">
+              Hats
+            </a>
+          </Link>
+          <Link href="/hats">
+            <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4">
+              Hats
+            </a>
+          </Link>
+          <Link href="/hats">
+            <a className="p-5 hover:text-green-500 ease-in duration-200 w-1/4 mr-auto">
+              Hats
+            </a>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
