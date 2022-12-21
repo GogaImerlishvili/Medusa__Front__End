9139411430b7efa48e5fd9cfa60543dcbc449e2a
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-12-21 23:41:41 +0400

    finish search and refactor, format documents

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -10,56 +10,53 @@ import styles from "./images.module.css"
 const Hero = () => {
   return (
     <>
-    <div className="h-[90vh] w-full relative">
-      <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
-        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Summer styles are finally here!! goga && kaxa
-        </h1>
-        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This year, our new summer collection will shelter you from the harsh
-          elements of a world that doesn&apos;t care if you live or die.
-        </p>
-        <UnderlineLink href="/store">Explore products</UnderlineLink>
+      <div className="h-[90vh] w-full relative">
+        <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+          <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
+            Summer styles are finally here!! goga && kaxa
+          </h1>
+          <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
+            This year, our new summer collection will shelter you from the harsh
+            elements of a world that doesn&apos;t care if you live or die.
+          </p>
+          <UnderlineLink href="/store">Explore products</UnderlineLink>
+        </div>
+        <Image
+          src="/hero.jpg"
+          layout="fill"
+          loading="eager"
+          priority={true}
+          quality={90}
+          objectFit="cover"
+          alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
+          className="absolute inset-0"
+          draggable="false"
+        />
       </div>
-      <Image
-        src="/hero.jpg"
-        layout="fill"
-        loading="eager"
-        priority={true}
-        quality={90}
-        objectFit="cover"
-        alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
-        className="absolute inset-0"
-        draggable="false"
-      />
-    </div>
-    <div className="h-[38vh] w-[90%] ml-[5%] relative  mt-4">
-    <ul className="grid grid-cols-5 small:grid-flow-col gap-x-2 ">
-      <li key={"1"}>
-      <Image src={menClothes} alt="men clothes" height={366} />
-      <h1 className="title">ფასდაკლება</h1>
-      </li>
-      <li key={"2"}>
-      <Image src={newCollection} alt="new collection"  height={280} />
-      <h1 className="title">საშემოდგომო კოლექცია</h1>
-      </li>
-      <li key={"2"}>
-      <Image src={sales} alt="new collection"   />
-      <h1 className="title">ონლაინ ექკლუზივი</h1>
-      </li>
-      <li key={"2"}>
-      <Image src={onlineExcl} alt="new collection" height={600}  />
-      <h1 className="title">ჩანთები და აქსესუარები</h1>
-      </li>
-      <li key={"2"}>
-      <Image src={fashion} alt="new collection"  height={800}/>
-      <h1 className={styles.title1}>ახალი კოლექცია</h1>
-      </li>
-     
-
-    </ul>
-        
-    </div>
+      <div className="h-[38vh] w-[90%] ml-[5%] relative  mt-4">
+        <ul className="grid grid-cols-5 small:grid-flow-col gap-x-2 ">
+          <li key={"1"}>
+            <Image src={menClothes} alt="men clothes" height={366} />
+            <h1 className="title">ფასდაკლება</h1>
+          </li>
+          <li key={"2"}>
+            <Image src={newCollection} alt="new collection" height={280} />
+            <h1 className="title">საშემოდგომო კოლექცია</h1>
+          </li>
+          <li key={"2"}>
+            <Image src={sales} alt="new collection" />
+            <h1 className="title">ონლაინ ექკლუზივი</h1>
+          </li>
+          <li key={"2"}>
+            <Image src={onlineExcl} alt="new collection" height={600} />
+            <h1 className="title">ჩანთები და აქსესუარები</h1>
+          </li>
+          <li key={"2"}>
+            <Image src={fashion} alt="new collection" height={800} />
+            <h1 className={styles.title1}>ახალი კოლექცია</h1>
+          </li>
+        </ul>
+      </div>
     </>
   )
 }
--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -9,14 +9,7 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { BsPersonPlusFill } from "react-icons/bs"
-import { searchClient, SEARCH_INDEX_NAME } from "@lib/search-client"
-import { InstantSearch, Hits } from "react-instantsearch-hooks-web"
-import Hit from "@modules/search/components/hit"
-import Search from "@modules/common/icons/search"
-/* import Search from "@modules/layout/components/search/search" */
-import DesktopHit from "@modules/search/components/desktop-hit"
-import DesktopHits from "@modules/search/components/desktop-hits"
-import SearchBox from "@modules/search/components/search-box"
+import Search from "@modules/layout/components/search"
 
 const Nav = () => {
   const { pathname } = useRouter()
@@ -79,29 +72,7 @@ const Nav = () => {
             </div>
           </div>
 
-          {/* search */}
-
-          <InstantSearch
-            indexName={SEARCH_INDEX_NAME}
-            searchClient={searchClient}
-          >
-            <div className="flex flex-col">
-              <div className="flex flex-col h-full">
-                <div className="w-full flex items-center gap-x-2 bg-gray-50 p-4">
-                  <Search />
-                  <SearchBox />
-                </div>
-              </div>
-              <div className="absolute top-16">
-                <Hits hitComponent={Hit} />
-              </div>
-            </div>
-
-            {/* <div className="overflow-y-scroll flex-1 no-scrollbar mt-6">
-                <DesktopHits hitComponent={DesktopHit} />
-                </div> */}
-          </InstantSearch>
-          {/* search */}
+          <Search />
 
           <div className="flex items-center gap-x-12 h-full flex-1 basis-0 justify-end">
             <div className="hidden small:flex items-center gap-x-6 h-full">
